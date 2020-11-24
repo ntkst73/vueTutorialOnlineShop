b8e5ecb15b6b5ac0bae10f7ddc0ba34ab339b69d
Кошарный Никита <ntkst73@mail.ru>
2020-11-24 12:53:11 +0400

    зависимости аксиос и json сервер

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello vue
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
